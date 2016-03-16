6a27bffa17824a1430cd7e9684d6b23411edd24b
Nexuist <nexuister@gmail.com>
2016-03-16 16:05:53 -0400

    Added action hooks to upgrade server node to most recent version if necessary
    This is needed to use ES6 classes

--- main.js
+++ main.js
@@ -2,5 +2,6 @@
 
 var Server = require("./server.js")
 
-var instance = new Server("127.0.0.1", 8080)
+//var instance = new Server("127.0.0.1", 8080)
+var instance = new Server(process.env.OPENSHIFT_NODEJS_IP, process.env.OPENSHIFT_NODEJS_PORT)
 instance.start()
