17bd251f56f4e1fbb5645b9724bc5f5bd63b0140
Shaun Newsum <shaun.newsum@gmail.com>
2017-06-01 23:51:59 -0700

    bind to PORT

--- main.js
+++ main.js
@@ -2,5 +2,5 @@
 
 var Server = require("./server.js")
 
-var instance = new Server(process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.TRACKING_ID || null)
+var instance = new Server(process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", process.env.PORT || 8080, process.env.TRACKING_ID || null)
 instance.start()
